0a404c21143356d41bc43c85c043fd20059c93fa
Link4real <simon@die-duerrs.de>
2021-11-01 13:01:40 +0100

    yeet

--- src/main/java/com/link/plushies/mod/Plushies/PandaPlushie.java
+++ src/main/java/com/link/plushies/mod/Plushies/PandaPlushie.java
@@ -28,13 +28,13 @@ public class PandaPlushie extends HorizontalFacingBlock {
         Direction dir = state.get(FACING);
         switch(dir) {
             case NORTH:
-                return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f);
+                return VoxelShapes.cuboid(0.2f, 0f, 0.2f, 0.8f, 0.6f, 0.7f);
             case SOUTH:
-                return VoxelShapes.cuboid(0.0f, 0.0f, 0.5f, 1.0f, 1.0f, 1.0f);
+                return VoxelShapes.cuboid(0.23f, 0f, 0.2f, 0.8f, 0.6f, 0.7f);
             case EAST:
-                return VoxelShapes.cuboid(0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+                return VoxelShapes.cuboid(0.22f, 0f, 0.2f, 0.8f, 0.6f, 0.7f);
             case WEST:
-                return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 0.5f, 1.0f, 1.0f);
+                return VoxelShapes.cuboid(0.3f, 0f, 0.3f, 0.8f, 0.6f, 0.7f);
             default:
                 return VoxelShapes.fullCube();
         }
