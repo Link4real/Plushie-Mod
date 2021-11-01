ff559a707f501e075eb3f7cc0126857115a8399e
Link4real <simon@die-duerrs.de>
2021-11-01 12:08:03 +0100

    omfg

--- src/main/java/com/link/plushies/mod/Main.java
+++ src/main/java/com/link/plushies/mod/Main.java
@@ -4,17 +4,7 @@ import com.link.plushies.mod.registry.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
-
-
-
-
     public static final String MOD_ID = "plushies";
-
-
-
-
-
-
 
     @Override
     public void onInitialize() {
--- src/main/java/com/link/plushies/mod/registry/ModBlocks.java
+++ src/main/java/com/link/plushies/mod/registry/ModBlocks.java
@@ -1,10 +1,28 @@
 package com.link.plushies.mod.registry;
 
+import com.link.plushies.mod.Plushies.PandaPlushie;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import static com.link.plushies.mod.Main.MOD_ID;
+
 public class ModBlocks {
 
 
+    public static final Block PANDA_PLUSHIE = new PandaPlushie(FabricBlockSettings.of(Material.WOOL).nonOpaque().sounds(BlockSoundGroup.WOOL));
+
     public static void registerItems() {
 
+
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "panda_plushie"), PANDA_PLUSHIE);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "panda_plushie"), new BlockItem(PANDA_PLUSHIE, new FabricItemSettings().maxCount(16).group(ItemGroup.DECORATIONS)));
 
     }
 }
