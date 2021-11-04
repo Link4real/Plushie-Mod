3b45855a561d22b157ca70af4c4a0c884c5dcda3
Link4real <simon@die-duerrs.de>
2021-11-04 16:00:09 +0100

    release very soon!

--- src/main/java/com/link/plushies/mod/Main.java
+++ src/main/java/com/link/plushies/mod/Main.java
@@ -2,9 +2,30 @@ package com.link.plushies.mod;
 
 import com.link.plushies.mod.registry.ModBlocks;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class Main implements ModInitializer {
     public static final String MOD_ID = "plushies";
+
+
+    //ItemGroup!
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
+            new Identifier("plushies", "main"))
+            .icon(() -> new ItemStack(ModBlocks.PANDA_PLUSHIE))
+            .appendItems(stacks -> {
+             stacks.add(new ItemStack(ModBlocks.PANDA_PLUSHIE));
+             stacks.add(new ItemStack(ModBlocks.SLIME_PLUSHIE));
+             stacks.add(new ItemStack(ModBlocks.RED_FOX_PLUSHIE));
+             stacks.add(new ItemStack(ModBlocks.WHITE_FOX_PLUSHIE));
+             stacks.add(new ItemStack(ModBlocks.DRAGON_PLUSHIE));
+            })
+            .build();
+    // ...
+
 
     @Override
     public void onInitialize() {
--- src/main/java/com/link/plushies/mod/registry/ModBlocks.java
+++ src/main/java/com/link/plushies/mod/registry/ModBlocks.java
@@ -1,5 +1,7 @@
 package com.link.plushies.mod.registry;
 
+import com.link.plushies.mod.Main;
+import com.link.plushies.mod.Plushies.DragonPlushie;
 import com.link.plushies.mod.Plushies.FoxPlushie;
 import com.link.plushies.mod.Plushies.PandaPlushie;
 import com.link.plushies.mod.Plushies.SlimePlushie;
@@ -11,6 +13,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 import static com.link.plushies.mod.Main.MOD_ID;
@@ -22,17 +25,20 @@ public class ModBlocks {
     public static final Block SLIME_PLUSHIE = new SlimePlushie(FabricBlockSettings.of(Material.WOOL).nonOpaque().sounds(BlockSoundGroup.WOOL).strength(0.6f));
     public static final Block RED_FOX_PLUSHIE = new FoxPlushie(FabricBlockSettings.of(Material.WOOL).nonOpaque().sounds(BlockSoundGroup.WOOL).strength(0.8f));
     public static final Block WHITE_FOX_PLUSHIE = new FoxPlushie(FabricBlockSettings.of(Material.WOOL).nonOpaque().sounds(BlockSoundGroup.WOOL).strength(0.8f));
+    public static final Block DRAGON_PLUSHIE = new DragonPlushie(FabricBlockSettings.of(Material.WOOL).nonOpaque().sounds(BlockSoundGroup.WOOL).strength(0.9f));
 
     public static void registerItems() {
 
 
         Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "panda_plushie"), PANDA_PLUSHIE);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "panda_plushie"), new BlockItem(PANDA_PLUSHIE, new FabricItemSettings().maxCount(16).group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "panda_plushie"), new BlockItem(PANDA_PLUSHIE, new FabricItemSettings().maxCount(16).group(Main.ITEM_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "slime_plushie"), SLIME_PLUSHIE);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "slime_plushie"), new BlockItem(SLIME_PLUSHIE, new FabricItemSettings().maxCount(16).group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "slime_plushie"), new BlockItem(SLIME_PLUSHIE, new FabricItemSettings().maxCount(16).group(Main.ITEM_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "red_fox_plushie"), RED_FOX_PLUSHIE);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "red_fox_plushie"), new BlockItem(RED_FOX_PLUSHIE, new FabricItemSettings().maxCount(16).group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "red_fox_plushie"), new BlockItem(RED_FOX_PLUSHIE, new FabricItemSettings().maxCount(16).group(Main.ITEM_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "white_fox_plushie"), WHITE_FOX_PLUSHIE);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "white_fox_plushie"), new BlockItem(WHITE_FOX_PLUSHIE, new FabricItemSettings().maxCount(16).group(ItemGroup.DECORATIONS)));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "white_fox_plushie"), new BlockItem(WHITE_FOX_PLUSHIE, new FabricItemSettings().maxCount(16).group(Main.ITEM_GROUP)));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "dragon_plushie"), DRAGON_PLUSHIE);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "dragon_plushie"), new BlockItem(DRAGON_PLUSHIE, new FabricItemSettings().rarity(Rarity.EPIC).maxCount(16).group(Main.ITEM_GROUP)));
     }
 }
