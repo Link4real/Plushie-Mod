a19c487ad5e29b1f4e32993bffc560660ac80886
söri <33805688+CrashedLife@users.noreply.github.com>
2023-11-05 23:25:49 +0100

    Replaced statement lambda with expression lambda in Plushies.java

--- src/main/java/com/link/plushies/Plushies.java
+++ src/main/java/com/link/plushies/Plushies.java
@@ -23,32 +23,30 @@ public class Plushies {
 
     @SubscribeEvent
     public void registerCreativeTab(CreativeModeTabEvent.Register event) {
-        event.registerCreativeModeTab(new ResourceLocation(MOD_ID), builder -> {
-            builder.title(Component.literal("Plushies"))
-                    .icon(() -> new ItemStack(Items.SNOW_GOLEM.get()))
-                    .displayItems((enabledFlags, tabOutput, hasPermissions) -> {
-                        tabOutput.accept(Items.SNOW_GOLEM.get());
-                        tabOutput.accept(Items.AXOLOTL.get());
-                        tabOutput.accept(Items.BEE.get());
-                        tabOutput.accept(Items.CAT.get());
-                        tabOutput.accept(Items.COW.get());
-                        tabOutput.accept(Items.DOLPHIN.get());
-                        tabOutput.accept(Items.DRAGON.get());
-                        tabOutput.accept(Items.ENDERMAN.get());
-                        tabOutput.accept(Items.FROG.get());
-                        tabOutput.accept(Items.GOAT.get());
-                        tabOutput.accept(Items.LLAMA.get());
-                        tabOutput.accept(Items.SLIME.get());
-                        tabOutput.accept(Items.MAGMA_CUBE.get());
-                        tabOutput.accept(Items.MOOSHROOM.get());
-                        tabOutput.accept(Items.PANDA.get());
-                        tabOutput.accept(Items.PARROT.get());
-                        tabOutput.accept(Items.RED_FOX.get());
-                        tabOutput.accept(Items.WHITE_FOX.get());
-                        tabOutput.accept(Items.WARDEN.get());
-                        tabOutput.accept(Items.SHEEP.get());
-                        tabOutput.accept(Items.TURTLE.get());
-                    }).build();
-        });
+        event.registerCreativeModeTab(new ResourceLocation(MOD_ID), builder -> builder.title(Component.literal("Plushies"))
+                .icon(() -> new ItemStack(Items.SNOW_GOLEM.get()))
+                .displayItems((enabledFlags, tabOutput, hasPermissions) -> {
+                    tabOutput.accept(Items.SNOW_GOLEM.get());
+                    tabOutput.accept(Items.AXOLOTL.get());
+                    tabOutput.accept(Items.BEE.get());
+                    tabOutput.accept(Items.CAT.get());
+                    tabOutput.accept(Items.COW.get());
+                    tabOutput.accept(Items.DOLPHIN.get());
+                    tabOutput.accept(Items.DRAGON.get());
+                    tabOutput.accept(Items.ENDERMAN.get());
+                    tabOutput.accept(Items.FROG.get());
+                    tabOutput.accept(Items.GOAT.get());
+                    tabOutput.accept(Items.LLAMA.get());
+                    tabOutput.accept(Items.SLIME.get());
+                    tabOutput.accept(Items.MAGMA_CUBE.get());
+                    tabOutput.accept(Items.MOOSHROOM.get());
+                    tabOutput.accept(Items.PANDA.get());
+                    tabOutput.accept(Items.PARROT.get());
+                    tabOutput.accept(Items.RED_FOX.get());
+                    tabOutput.accept(Items.WHITE_FOX.get());
+                    tabOutput.accept(Items.WARDEN.get());
+                    tabOutput.accept(Items.SHEEP.get());
+                    tabOutput.accept(Items.TURTLE.get());
+                }).build());
     }
 }
