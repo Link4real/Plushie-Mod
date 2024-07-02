b8b2734f09e6d7cb99694b9c83dda5a41f250943
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-07-02 19:18:02 +0200

    fully migrate to 1.21+neoforge

---
+++ neoforge/src/main/java/com/link/plushies/forge/PlushiesModForge.java
@@ -0,0 +1,12 @@
+package com.link.plushies.forge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.link.plushies.PlushiesMod;
+
+@Mod(PlushiesMod.MOD_ID)
+public class PlushiesModForge {
+    public PlushiesModForge() {
+        PlushiesMod.init();
+    }
+}
